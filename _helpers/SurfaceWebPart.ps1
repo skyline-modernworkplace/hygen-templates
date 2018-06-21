01e8fa1db1e618dc4c8eeca3ccaed53fd2ec03ef
Petersen <apetersen@skylinetechnologies.com>
2018-06-21 15:36:10 -0500

    switched to rimraf

--- _helpers/SurfaceWebPart.ps1
+++ _helpers/SurfaceWebPart.ps1
@@ -7,7 +7,7 @@ $tempPath = Join-Path -Path $pwd -ChildPath ("/src/webparts/_" + $name)
 $nestedPath = Join-Path -Path $parentPath -ChildPath ("/webparts/" + $name)
 
 if (Test-Path $nestedPath) {
-    Move-Item $nestedPath $tempPath
-    Remove-Item $parentPath -Force -Recurse
-    Move-Item $tempPath $parentPath
+    mv $nestedPath $tempPath
+    rimraf $parentPath
+    mv $tempPath $parentPath
 }
