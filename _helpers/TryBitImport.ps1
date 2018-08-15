76095a9b5d061b61d10ea8d3d69f50411cb13308
Petersen <apetersen@skylinetechnologies.com>
2018-08-15 13:19:14 -0500

    updated trybitimport to test for file counts so it triggers on empty folders

--- _helpers/TryBitImport.ps1
+++ _helpers/TryBitImport.ps1
@@ -3,9 +3,16 @@ param(
     $Id
 )
 
-$path = Join-Path -Path $pwd  -ChildPath ("/src/" + $Id)
-
-if (Test-Path $path) {
+Function CheckExists($SrcPath) {
+    $path = Join-Path -Path $pwd  -ChildPath ("/src/" + $SrcPath)
+    if (Test-Path $path) {
+        $itemCount = (Get-ChildItem $path | Measure-Object).Count
+        return $itemCount -gt 0
+    } else {
+        return false
+    }
+}
+if (CheckExists -SrcPath $Id) {
     Write-Host "BIT Component Already Exists"
 } else {
     Write-Host "IMPORTING BIT COMPONENT"
