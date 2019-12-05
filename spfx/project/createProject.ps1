9b6ac9caef4db43ffb509877ed1c3d23238abcd1
Petersen <apetersen@skylinetechnologies.com>
2019-12-05 10:29:44 -0600

    updated createProject file

--- spfx/project/createProject.ps1
+++ spfx/project/createProject.ps1
@@ -1,66 +1,55 @@
-# CREATE PROJECT FOLDER
-$projectFolder = Read-Host "What is the project folder name?"
+Param(
+    $Name
+)
+$projectFolder = $Name;
 
-# BEGIN: ASK IF IT SHOULD CREATE A SPARK SITE
-$shouldCreateSiteInput = Read-Host "Create new SPARK Communication Site? ( y / n )" 
-Switch ($shouldCreateSiteInput) { 
-    Y { $shouldCreateSite = $true } 
-    N { $shouldCreateSite = $false } 
-    Default { $shouldCreateSite = $false } 
+if (!$projectFolder) {
+    $projectFolder = Read-Host "What is the project folder name?"
 }
-
-if ($shouldCreateSite) {
-    $siteTitle = Read-Host "What is the Site Title?"
-    $sitePath = Read-Host "What is the Site Path (/sites/{PATH})?"
-    $siteUrl = "https://skylinespark.sharepoint.com/sites/" + $sitePath
-}
-# END: ASK IF IT SHOULD CREATE A SPARK SITE
-
-# Create a new folder for the project and cd into it
 mkdir $projectFolder
 Set-Location $projectFolder
 
-# Initialize GIT
-git init
+# SETUP MICROSOFT GENERATOR CONFIG
+$yeomanConfig = "{
+`"@microsoft/generator-sharepoint`": {
+    `"environment`": `"spo`",
+    `"componentType`": `"webpart`",
+    `"framework`": `"react`",
+    `"version`": `"1.9.1`",
+    `"packageManager`": `"npm`",
+    `"whichFolder`": `"current`",
+    `"skipFeatureDeployment`": true,
+    `"isDomainIsolated`": false
+  }
+}"
+Set-Content -Path ".yo-rc.json" -Value $yeomanConfig;
 
+git init
 # SETUP HYGEN TEMPLATES
 git clone https://skyline.visualstudio.com/Skyline-Portals-Reusables/_git/hygen-templates _templates
 Set-Location _templates
 npm install
 Set-Location ..
 
-# INSTALL & RUN MICROSOFT GENERATOR
-npm install @microsoft/generator-sharepoint 
-yo @microsoft/sharepoint --framework "react" --skip-install --skip-feature-deployment true --whichFolder "current" --component-type "webpart" --component-name "DeleteME" --component-description "DeleteME web part" 
+# RUN PNP GENERATOR
+yo @pnp/spfx --skip-install --environment "spo"
 
-# Make a GIT commit
 git add -A 
-git commit -m "generated microsoft project"
+git commit -m "Generated PnP SPFx Project project"
 
 # Delete the webpart the Microsoft's Generator created
-rimraf src/webparts/deleteMe
+rimraf src/webparts/helloWorld
 
 # Copy in core source code
 Copy-Item .\_templates\_static\* .\ -Force -Recurse
 
-# Run the hygen project generator (add npm scripts, setup /tasks etc...)
 hygen spfx project
-
 # Generate an example webpart so the bundle isn't empty
 hygen webparts blank --name "Example" --displayName "Example (Custom)" --description "Example Web Part"
 
-# GIT Commit
 git add -A 
 git commit -m "Added core source code and generated example webpart"
-
-# Create the SPARK Communication site if user said to
-if ($shouldCreateSite) {
-    Write-Host "Creating Site: " $siteUrl
-    Connect-PnPOnline https://skylinespark.sharepoint.com
-    New-PnPSite -Type CommunicationSite -Title $siteTitle -Url $siteUrl
-    Connect-PnPOnline $siteUrl
-    Add-PnPSiteCollectionAdmin -Owners @( "sotto@skylinespark.onmicrosoft.com", "apetersen@skylinespark.onmicrosoft.com", "wspiering@skylinespark.onmicrosoft.com" )
-    Add-PnPSiteCollectionAppCatalog -Site $siteUrl
-}
-
 npm install
+
+# TODO: Fix duplicate react types. 
+# TODO: Yank the localized webpart strings from the microsoft web part
