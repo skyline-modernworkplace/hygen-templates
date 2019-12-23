a05fe84c9d900aeab16c15c722b13dc3a91d7641
Petersen <apetersen@skylinetechnologies.com>
2019-12-23 15:28:48 -0600

    added script to copy ui toolkit

---
+++ _helpers/copyUIToolkit.ps1
@@ -0,0 +1,8 @@
+
+git clone https://skyline.visualstudio.com/DefaultCollection/Skyline-Portals-Reusables/_git/ip-designsystem _temp
+$destBase = "src/ui-toolkit/";
+$srcBase = "_temp\\portalsdev-ui-toolkit\src\\"
+Copy-Item -Path ($srcBase + "hooks") -Destination ($destBase + "hooks") -Force -Recurse
+Copy-Item -Path ($srcBase + "components") -Destination ($destBase ) -Force -Recurse
+Copy-Item -Path ($srcBase + "core") -Destination ($destBase) -Force -Recurse
+rimraf _temp
