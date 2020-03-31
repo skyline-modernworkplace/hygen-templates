8a070ad163000c8de4a6dd09249a5ba99b680994
Petersen <apetersen@skylinetechnologies.com>
2020-03-31 12:48:04 -0500

    fixed copyUITookit to copy the entire folder src/ui-toolkit

--- _helpers/copyUIToolkit.ps1
+++ _helpers/copyUIToolkit.ps1
@@ -1,9 +1,6 @@
 
 git clone https://skyline.visualstudio.com/DefaultCollection/Skyline-Portals-Reusables/_git/ip-designsystem _temp
-$destBase = "src/ui-toolkit/";
-$srcBase = "_temp\\portalsdev-ui-toolkit\src\\"
-Copy-Item -Path ($srcBase + "hooks") -Destination ($destBase) -Force -Recurse
-Copy-Item -Path ($srcBase + "components") -Destination ($destBase ) -Force -Recurse
-Copy-Item -Path ($srcBase + "core") -Destination ($destBase) -Force -Recurse
-Copy-Item -Path ($srcBase + "propertyfields") -Destination ($destBase) -Force -Recurse
+$destBase = "src";
+$src = "_temp\\portalsdev-ui-toolkit\src\ui-toolkit"
+Copy-Item -Path $src -Destination $destBase -Force -Recurse
 rimraf _temp
