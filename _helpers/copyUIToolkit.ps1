41c8383726a21dac57a971ae5eb788ce34ab0875
Petersen <apetersen@skylinetechnologies.com>
2020-06-02 09:30:12 -0500

    updated copyUITOolkit to use ui-toolkit folder

--- _helpers/copyUIToolkit.ps1
+++ _helpers/copyUIToolkit.ps1
@@ -1,6 +1,7 @@
 
 git clone https://skyline.visualstudio.com/DefaultCollection/Skyline-Portals-Reusables/_git/ip-designsystem _temp
 $destBase = "src";
-$src = "_temp\\portalsdev-ui-toolkit\src\ui-toolkit"
+$src = "_temp\\ui-toolkit\src\ui-toolkit"
+
 Copy-Item -Path $src -Destination $destBase -Force -Recurse
 rimraf _temp
